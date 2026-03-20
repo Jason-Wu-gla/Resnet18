953b665bb234220e38c02c43d16c77fd9818dd5c
unknown <2050430643@qq.com>
2026-03-20 22:25:29 +0800

    revise

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from test import test
 from train import train
 from model import Model
 import torch
-from plot import plot
+from plot import plot_training_loss, plot_validation_loss, plot_success_rate, plot_validation_success_rate
 
 model = Model()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -11,7 +11,8 @@ model = model.to(device)
 if device == 'cuda':
     model = torch.nn.DataParallel(model)
 
-model, loss_history = train(model, 200)
-plot(loss_history)
-
-"""111"""
+model, loss_history, success_history, val_loss_history, val_success_history = train(model, 10)
+plot_training_loss(loss_history)
+plot_validation_loss(val_loss_history)
+plot_success_rate(success_history)  
+plot_validation_success_rate(val_success_history)
